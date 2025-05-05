2daa99b8720bfe5502d30c9233ac38d3a819846e
lexuantruongtv <truongle2472004@gmail.com>
2025-05-05 18:15:38 +0700

    phục hồi

--- api/server.js
+++ api/server.js
@@ -4,8 +4,7 @@ import cors from 'cors';
 import dotenv from 'dotenv';
 import connectDB from './config/db.js';
 import scheduleRoutes from './routes/schedules.js';
-import userListRoutes from './routes/users.js';
-import userRoutes from './routes/userRoutes.js';
+import userRoutes from './routes/users.js';
 
 dotenv.config();
 connectDB();
@@ -15,8 +14,7 @@ app.use(cors());
 app.use(express.json());
 
 app.use('/api/schedules', scheduleRoutes);
-app.use('/api/user-list', userListRoutes);
-app.use('/api/user', userRoutes);
+app.use('/api/users', userRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
