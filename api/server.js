21e66aa245d3114d03781d41a5e94a7fec8a1e97
huetrinh204 <trinhfokko@gmail.com>
2025-07-03 18:12:36 +0700

    update sv

--- api/server.js
+++ api/server.js
@@ -9,12 +9,7 @@ import scheduleRoutes from './routes/scheduleRoutes.js';
 dotenv.config();
 connectDB();
 
-const app = express();
-app.use(cors());
-app.use(express.json());
 
-app.use('/api/users', userRoutes);
-app.use('/api/schedules', scheduleRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`🚀✅ Server đang chạy trên cổng ${PORT}`));
