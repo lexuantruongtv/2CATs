aa8cef5fc35155e4539d507a03101661f9aeaa6e
lexuantruongtv <truongle2472004@gmail.com>
2025-07-03 15:41:41 +0700

    update lịch

--- api/models/scheduleModel.js
+++ api/models/scheduleModel.js
@@ -1,11 +1,6 @@
 import mongoose from 'mongoose';
 
 const scheduleSchema = new mongoose.Schema({
-    user: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'User',
-        required: true
-    },
     title: { type: String, required: true },
     time: { type: Date, required: true },
     description: { type: String },
--- api/models/userModel.js
+++ api/models/userModel.js
@@ -15,6 +15,11 @@ const userSchema = new mongoose.Schema({
     type: String,
     required: true,
   },
+  scheduleSchema: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Schedule',
+        required: true
+    },
 });
 
 const User = mongoose.model('User', userSchema);
